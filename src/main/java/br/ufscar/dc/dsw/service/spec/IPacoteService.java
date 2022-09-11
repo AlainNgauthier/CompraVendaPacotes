b73f429db538d2a372397600c3796dfa3c2c5085
Alain Gauthier Ndamwey Djamba <alain.djamba@ifood.com.br>
2022-09-11 12:29:43 -0300

    feat: add empty files to build rest controllers

--- src/main/java/br/ufscar/dc/dsw/service/spec/IPacoteService.java
+++ src/main/java/br/ufscar/dc/dsw/service/spec/IPacoteService.java
@@ -5,6 +5,8 @@ import br.ufscar.dc.dsw.domain.Pacote;
 
 public interface IPacoteService {
     Pacote buscarPorId(Long id);
+    Pacote buscarTodosPorAgencia(Long id);
+    Pacote buscarTodosPorDestinos(String destinos);
     List<Pacote> buscarTodos();
 	void salvar(Pacote pacote);
 	void excluir(Long id);
